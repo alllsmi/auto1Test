a5ba1b20cde0e3fff8fe4e216a22faa840c1fff3
Alexander Smirnov <alexander.smirnov404@gmail.com>
2018-08-07 15:14:38 +0300

    set browser resolution

--- src/test/java/infrastructure/DriverFactory.java
+++ src/test/java/infrastructure/DriverFactory.java
@@ -17,18 +17,19 @@ public class DriverFactory {
         if (isWindows){
             System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
             driver = new ChromeDriver();
+            driver.manage().window().maximize();
         }
         else {
             System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless");
             options.addArguments("--no-sandbox");
+            options.addArguments("window-size=1920,1080");
             options.addArguments("--disable-dev-shm-usage");
             driver = new ChromeDriver(options);
             System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         }
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
         return driver;
     }
 }
