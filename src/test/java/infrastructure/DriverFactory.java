624941d85032b5d55bc1b3845347d3c5e0556638
Alexander Smirnov <alexander.smirnov404@gmail.com>
2018-08-07 14:16:45 +0300

    fix linux chromedriver options

--- src/test/java/infrastructure/DriverFactory.java
+++ src/test/java/infrastructure/DriverFactory.java
@@ -2,6 +2,7 @@ package infrastructure;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,13 +13,20 @@ public class DriverFactory {
     }
 
     private static ChromeDriver createNewChrome(boolean isWindows) {
+        ChromeDriver driver;
         if (isWindows){
             System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+            driver = new ChromeDriver();
         }
         else {
             System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            driver = new ChromeDriver(options);
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
         }
-        ChromeDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         return driver;
